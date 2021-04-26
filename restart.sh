42b153243432c7e3756654aa14417e95d9028182
Enstein Dread <edread@ox-r5.msk.21-school.ru>
2021-04-26 21:04:46 +0300

    add nginx

---
+++ restart.sh
@@ -0,0 +1,2 @@
+minikube delete
+./setup.sh
---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+minikube start --vm-driver=virtualbox
+eval $(minikube docker-env)
+minikube addons enable metallb
+
+kubectl apply -f ./srcs/configmap.yaml
+
+#nginx
+docker build -t nginx_image ./srcs/nginx 
+kubectl apply -f ./srcs/nginx/nginx.yaml
+
+#wordpress
+#docker build -t wordpress_image ./srcs/wordpress
+#kubectl apply -f ./srcs/wordpress/wordpress.yaml
+
+#phpmyadmin
+#docker build -t php_image ./srcs/phpmyadmin
+#kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+minikube dashboard
